4470cbf789714db9b11e7992c6e14f541ad3e0cb
lothrazar <samson.bassett@gmail.com>
2021-01-22 11:23:09 -0800

    db test

--- services/database.php
+++ services/database.php
@@ -2,21 +2,19 @@
 // used to get mysql database connection
 class DatabaseService {
 
-    $secrets  = json_decode(file_get_contents("../secrets.json"), true);
-
-
-
     public $host = "localhost:3306";
     public $name = "lothraza_db";
     public $user = "lothraza_admin";
-    public $password = $secrets["mysql_password"];
+    public $password = "";
     private $connection;
 
     public function get() {
 
+        $this->secrets = json_decode(file_get_contents("../secrets.json"), true);
+        $this->password = $this->secrets["mysql_password"];
         $this->connection = null;
 
-        try{
+        try {
             $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->name, $this->user, $this->password);
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
